b757c87d3e7987b4938884017183e754e9e4a344
kittimaratana <kittima.ratana@gmail.com>
2024-06-25 16:41:37 -0400

    Add color and image in mobile layout

--- src/components/AboutBio/AboutBio.js
+++ src/components/AboutBio/AboutBio.js
@@ -1,9 +1,21 @@
 import "./AboutBio.scss";
+import profile from "../../assets/images/profile.jpg";
 
 function AboutBio() {
 
     return (
-        <p>Bio</p>
+        <section class="about-bio">
+            <section class="about-bio__center-wrap">
+                <h1 class="about-bio__title">I'm Kittima</h1>
+                <section class="about-bio__image-container">
+                    <img src={profile} alt="profile image" class="about-bio__image"/>
+                </section>
+                <section class="about-bio__intro-container">
+                    <h2 class="about-bio__intro-title">I'm a perfect blend between software engineering, data scientist and business mindset based in Toronto</h2>
+                    <p class="about-bio__intro-bio">Talking about my bio...........</p>
+                </section>
+            </section>
+        </section>
     )
 }
 
