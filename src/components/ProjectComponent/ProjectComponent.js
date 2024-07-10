7cd5c6d15254582ff3ebfea17f283786e5f7249d
kittimaratana <kittima.ratana@gmail.com>
2024-07-10 16:43:38 -0400

    Add multer to allow client side access to images

--- src/components/ProjectComponent/ProjectComponent.js
+++ src/components/ProjectComponent/ProjectComponent.js
@@ -1,10 +1,12 @@
 import "./ProjectComponent.scss";
 import {LinkedIn} from '@mui/icons-material';
+import {BASE_URL} from "../../utils/constant-variables";
 
 //need to do multer
 function ProjectComponent({title,description,techStack,image}) {
     return (
         <section className="article__item-container">
+            <img className="video__image" src={`${BASE_URL}/${image}`} />
             <section className="article__info">
                 <p className="article__title">{title}</p>
             </section>
