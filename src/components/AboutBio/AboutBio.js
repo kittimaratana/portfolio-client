20d12b17d794f5a9daf8ec90d5428477fc0c7c49
kittimaratana <kittima.ratana@gmail.com>
2024-07-28 14:22:56 +0200

    Add work experience HTML and SASS in Mobile Format

--- src/components/AboutBio/AboutBio.js
+++ src/components/AboutBio/AboutBio.js
@@ -12,12 +12,9 @@ function AboutBio() {
                         <img src={profile} alt="profile image" class="about-bio__image"/>
                     </section>
                     <section class="about-bio__intro-container">
-                        <h2 class="about-bio__intro-title">I'm a perfect blend between software engineering, data scientist and business mindset based in Toronto</h2>
-                        <p class="about-bio__intro-bio">Transitioning from data science to software engineering is my latest adventure. I graduated from the University of Toronto in industrial engineering and was amongst the first cohorts to specialize in artificial intelligence. I love problem-solving, whether it's participating in case competitions, tackling LeetCode questions, or brainstorming ideas to optimize day-to-day work with analytics, algorithms, and machine learning. ⚙️</p>
-                        <p class="about-bio__intro-bio">Throughout my career, I've had some cool and unique opportunities. My internship experiences include researching abroad in Denmark 🇩🇰, working as a full-stack developer intern, and working cross-functionally to revamp how weather alerts are deployed to Canadians by designing the system architecture.</p>
-                        <p class="about-bio__intro-bio">Post-graduation, my role evolved from a junior data analyst to a data scientist at the same rate that my love for programming increased. I was part of the pioneering team at Universal Music Canada 🎧, advancing music and tech by building products in the analytics and machine learning space.</p>
-                        <p class="about-bio__intro-bio">I was offered a fully funded bootcamp through Brainstation 👩‍💻, and I decided to partake in the software engineering stream. As I graduate, I am excited to see what software engineering opportunities come my way.</p>
-                        <p class="about-bio__intro-bio">In my spare time, I love to travel, workout, and sing. I'm also passionate about women's community building 👭. I've participated in Women in STEM conferences, raised money for literacy, and am currently a member at Monday Girl, which is an organization that empowers women through fostering lasting and supportive community.</p>
+                        <h2 class="about-bio__intro-title">I'm a lover in all things programming and machine learning with a creative and growth mindset.</h2>
+                        <p class="about-bio__intro-bio">Over the past 7 years, I've worked and interned in various areas of technology ⚙️ from full stack developer, analyst, and data scientist in numerous industries.</p>
+                        <p class="about-bio__intro-bio">After completing additional software engineering studies, I am now looking for software engineering roles with higher development focus.👩‍💻</p>
                     </section>
                 </section>
             </section>
--- src/components/AboutWork/AboutWork.js
+++ src/components/AboutWork/AboutWork.js
@@ -1,50 +1,167 @@
 import "./AboutWork.scss";
+import {useState} from "react";
+import {Lyrics, Headphones, Public, School, Place} from '@mui/icons-material';
 
 function AboutWork() {
+    const [workClick1, setworkClick1] = useState(false);
+    const [workClick2, setworkClick2] = useState(false);
+    const [workClick3, setworkClick3] = useState(false);
+    const [workClick4, setworkClick4] = useState(false);
+
+    const handleWorkClick1 = () => {
+        setworkClick1(!workClick1)
+    }
+    const handleWorkClick2 = () => {
+        setworkClick2(!workClick2)
+    }
+    const handleWorkClick3 = () => {
+        setworkClick3(!workClick3)
+    }
+    const handleWorkClick4 = () => {
+        setworkClick4(!workClick4)
+    }
 
     return (
         <section class="about-work">
             <section class="about-work__center-wrap">
-                <h1 class="about-work__title">Work Experiences Include</h1>
+                <h1 class="about-work__title">Professional Experiences</h1>
                 <section class="about-work__container">
                     <section class="about-work__item">
-                        <section class="about-work__work">Universal Music Group</section>
-                        <section class="about-work__role">Data Scientist</section>
-                        <section class="about-work__period">Mar 2022 - Jan 2024</section>
-                        <ul class="about-work__detail-container">
-                            <li class="about-work__detail">Spearheaded the design, build, and maintenance of end-to-end analytics applications using Python, Selenium, HTML, CSS, SQL, GCP, and API data, saving 1,600+ hours/year and exceeding team revenue goals by 16%</li>
-                            <li class="about-work__detail">Led a company-wide ML project, collaborating with engineering and analytics teams to scale Python scripts via Git, SQL, GCP, and Docker, presenting insights that shaped executives' 2024 strategies at the company forum</li>
-                            <li class="about-work__detail">Recognized 2x with the Optimistic Award within the Digital Strategy Department in 2022 and 2023</li>
-                        </ul>
+                        <section class="about-work__preview-container">
+                            <section class="about-work__preview">
+                                <p>Data Scientist @</p>
+                                <section class="about-work__company-container">
+                                    <p class="about-work__company">Universal Music Group</p>
+                                    <Lyrics style={{color: 'white'}}/>
+                                </section>
+                            </section>
+                            {!workClick1 && (
+                                <p class="about-work__preview-logo about-work__preview-logo--off" onClick={handleWorkClick1}>+</p>
+                            )}
+                            {workClick1 && (
+                                <p class="about-work__preview-logo about-work__preview-logo--on" onClick={handleWorkClick1}>-</p>
+                            )}
+                        </section>
                     </section>
+                    {workClick1 && (
+                        <section class="about-work__detail-container">
+                            <section class="about-work__location-container">
+                                <Place style={{color: 'white'}}/>
+                                <p class="about-work__location">Toronto, Canada</p>
+                            </section>
+                            <p class="about-work__role">Core Responsibilities: Automation, Machine Learning Deployment, Analytical Tools Development, Industry Deep Dives, Data Engineering</p>
+                            <section class="about-work__skill-container">
+                                <p class="about-work__skill">Python</p>
+                                <p class="about-work__skill">SQL</p>
+                                <p class="about-work__skill">Selenium</p>
+                                <p class="about-work__skill">GCP</p>
+                                <p class="about-work__skill">Docker</p>
+                                <p class="about-work__skill">Git</p>
+                                <p class="about-work__skill">HTML</p>
+                            </section>
+                        </section>
+                    )}
                     <section class="about-work__item">
-                        <section class="about-work__work">Universal Music Group</section>
-                        <section class="about-work__role">Junior Data Analyst and Data Analyst</section>
-                        <section class="about-work__period">Jul 2020 - Feb 2022</section>
-                        <ul class="about-work__detail-container">
-                            <li class="about-work__detail">Collaborated with senior leadership and data engineering teams to develop automated tools using Python, SQL, and GCP, which saved $60,000/year in subscription costs and improved data processing efficiency</li>
-                            <li class="about-work__detail">Redesigned backend, database implementation, and revenue tools UI, helping team surpass financial targets by 5%</li>
-                            <li class="about-work__detail">Mentored junior teammates in coding, cloud, and analysis standards as first hire with team currently at 6 members</li>
-                        </ul>
+                        <section class="about-work__preview-container">
+                            <section class="about-work__preview">
+                                <p>Data Analyst Roles @</p>
+                                <section class="about-work__company-container">
+                                    <p class="about-work__company">Universal Music Group</p>
+                                    <Lyrics style={{color: 'white'}}/>
+                                </section>
+                            </section>
+                            {!workClick2 && (
+                                <p class="about-work__preview-logo about-work__preview-logo--off" onClick={handleWorkClick2}>+</p>
+                            )}
+                            {workClick2 && (
+                                <p class="about-work__preview-logo about-work__preview-logo--on" onClick={handleWorkClick2}>-</p>
+                            )}
+                        </section>
                     </section>
+                    {workClick2 && (
+                        <section class="about-work__detail-container">
+                            <section class="about-work__location-container">
+                                <Place style={{color: 'white'}}/>
+                                <p class="about-work__location">Toronto, Canada</p>
+                            </section>
+                            <p class="about-work__role">Core Responsibilities: Automation, Analytical Tools Development, Industry Deep Dives, Machine Learning</p>
+                            <section class="about-work__skill-container">
+                                <p class="about-work__skill">Python</p>
+                                <p class="about-work__skill">SQL</p>
+                                <p class="about-work__skill">GCP</p>
+                                <p class="about-work__skill">Excel</p>
+                                <p class="about-work__skill">Tableau</p>
+                                <p class="about-work__skill">Looker</p>
+                            </section>
+                        </section>
+                    )}
                     <section class="about-work__item">
-                        <section class="about-work__work">Environment Canada</section>
-                        <section class="about-work__role">Business Systems Analyst Intern</section>
-                        <section class="about-work__period">May 2018 - Dec 2019</section>
-                        <ul class="about-work__detail-container">
-                            <li class="about-work__detail">Crafted technical requirements and system architecture for weather application features, collaborating with 20+ members across development, business, and quality assurance (QA)</li>
-                            <li class="about-work__detail">Conducted root cause analysis for 30+ weather system bugs and designed testing protocols for software development, improving system accuracy by 5%</li>
-                        </ul>
+                        <section class="about-work__preview-container">
+                            <section class="about-work__preview">
+                                <p>Business Systems Analyst Intern @</p>
+                                <section class="about-work__company-container">
+                                    <p class="about-work__company">Environment Canada</p>
+                                    <Lyrics style={{color: 'white'}}/>
+                                </section>
+                            </section>
+                            {!workClick3 && (
+                                <p class="about-work__preview-logo about-work__preview-logo--off" onClick={handleWorkClick3}>+</p>
+                            )}
+                            {workClick3 && (
+                                <p class="about-work__preview-logo about-work__preview-logo--on" onClick={handleWorkClick3}>-</p>
+                            )}
+                        </section>
                     </section>
+                    {workClick3 && (
+                        <section class="about-work__detail-container">
+                            <section class="about-work__location-container">
+                                <Place style={{color: 'white'}}/>
+                                <p class="about-work__location">Toronto, Canada</p>
+                            </section>
+                            <p class="about-work__role">Core Responsibilities: Developing Canadian Weather Systems and Feature Integrations (Systems Engineering Perspective)</p>
+                            <section class="about-work__skill-container">
+                                <p class="about-work__skill">Data Modelling</p>
+                                <p class="about-work__skill">Process Optimization</p>
+                                <p class="about-work__skill">Debugging</p>
+                                <p class="about-work__skill">System Architecture</p>
+                                <p class="about-work__skill">Specifications Documents</p>
+                            </section>
+                        </section>
+                    )}
                     <section class="about-work__item">
-                        <section class="about-work__work">Top Olympiads</section>
-                        <section class="about-work__role">Full Stack Engineer Intern</section>
-                        <section class="about-work__period">May 2017 - Jun 2017</section>
-                        <ul class="about-work__detail-container">
-                            <li class="about-work__detail">Revamped tutoring website in 2 months using JavaScript, PHP, HTML, CSS, and MySQL GoDaddy databases</li>
-                            <li class="about-work__detail">Designed database architecture and UI for web pages, automating grading processes and aligning with feedback</li>
-                        </ul>
+                        <section class="about-work__preview-container">
+                            <section class="about-work__preview">
+                                <p>Full Stack Developer Intern @</p>
+                                <section class="about-work__company-container">
+                                    <p class="about-work__company">Top Olympiads</p>
+                                    <Lyrics style={{color: 'white'}}/>
+                                </section>
+                            </section>
+                            {!workClick4 && (
+                                <p class="about-work__preview-logo about-work__preview-logo--off" onClick={handleWorkClick4}>+</p>
+                            )}
+                            {workClick4 && (
+                                <p class="about-work__preview-logo about-work__preview-logo--on" onClick={handleWorkClick4}>-</p>
+                            )}
+                        </section>
                     </section>
+                    {workClick4 && (
+                        <section class="about-work__detail-container">
+                            <section class="about-work__location-container">
+                                <Place style={{color: 'white'}}/>
+                                <p class="about-work__location">Toronto, Canada</p>
+                            </section>
+                            <p class="about-work__role">Core Responsibilities: Frontend Development, Backend Development, Deployment</p>
+                            <section class="about-work__skill-container">
+                                <p class="about-work__skill">JavaScript</p>
+                                <p class="about-work__skill">SQL</p>
+                                <p class="about-work__skill">HTML</p>
+                                <p class="about-work__skill">CSS</p>
+                                <p class="about-work__skill">PHP</p>
+                                <p class="about-work__skill">GoDaddy</p>
+                            </section>
+                        </section>
+                    )}    
                 </section>
             </section>
         </section>
